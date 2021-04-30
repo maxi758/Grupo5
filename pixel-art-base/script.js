0e03b2d520044a9309e853dfaefd2c12733c30c1
franciCabrera <francabre97@gmail.com>
2021-04-29 22:20:20 -0300

    Agregamos parte tres del ejercicio

--- pixel-art-base/script.js
+++ pixel-art-base/script.js
@@ -1,15 +1,21 @@
 const btnAdd = document.getElementById('btnCrearCuadrilla');
 const container = document.getElementById('container');
+const inputColumna = document.getElementById('numero-columnas');
+const inputFilas = document.getElementById('numero-filas')
 
 
 btnAdd.addEventListener('click', function() {
-    for (let i = 0; i < 400; i++) {
+
+    container.innerHTML = '';
+    container.style.gridTemplateColumns=`repeat(${inputColumna.value}, 1fr)`;
+    const producto = parseInt(inputColumna.value * inputFilas.value);
+    
+    for (let i = 0; i < producto; i++) {
         const nuevoDiv = document.createElement('div');
         nuevoDiv.classList.add('caja');
         container.appendChild(nuevoDiv);
         nuevoDiv.addEventListener('click', function() {
             nuevoDiv.classList.toggle('seleccionado')
         });
-
-    }
+    }    
 });
