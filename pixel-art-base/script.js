d7aa3259b25cba2c12ba1f7a16302e2ce3d7fd77
franciCabrera <francabre97@gmail.com>
2021-05-02 10:18:30 -0300

    Agregue comentarios para una mejor lectura

--- pixel-art-base/script.js
+++ pixel-art-base/script.js
@@ -1,20 +1,27 @@
-const btnAdd = document.getElementById('btnCrearCuadrilla');
-const container = document.getElementById('container');
-const inputColumna = document.getElementById('numero-columnas');
-const inputFilas = document.getElementById('numero-filas')
+const btnAdd = document.getElementById('btnCrearCuadrilla'); //Trae el evento click
+const container = document.getElementById('container'); //Trae el div contenedor
+const inputColumna = document.getElementById('numero-columnas'); //Trae lo que ingresamos en el input de columnas
+const inputFilas = document.getElementById('numero-filas'); //Trae lo que ingresamos en el input de filas
 
 
 btnAdd.addEventListener('click', function() {
-
+    //Vaciar el html
     container.innerHTML = '';
+    //Estilo de css que crea el pixelArt
     container.style.gridTemplateColumns=`repeat(${inputColumna.value}, 1fr)`;
+    //Calculo del producto de filas y columnas
     const producto = parseInt(inputColumna.value * inputFilas.value);
-    
+    //Inicio del for
     for (let i = 0; i < producto; i++) {
+        //Crea el elemento div
         const nuevoDiv = document.createElement('div');
+        //Agrega al div una clase "caja"
         nuevoDiv.classList.add('caja');
+        //Agrega el div creado al contenedor
         container.appendChild(nuevoDiv);
+        //Creamos la funcion
         nuevoDiv.addEventListener('click', function() {
+            //Crea la clase "seleccionado" para que al hacer clic cambie el color
             nuevoDiv.classList.toggle('seleccionado')
         });
     }    
